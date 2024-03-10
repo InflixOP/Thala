6087a709904905701ff5b2f00bd6b4ec0c9cb864
AyushSingh0407 <114868218+AyushSingh0407@users.noreply.github.com>
2024-03-10 12:13:42 +0530

    updates

--- Backend/routes/creatorauthRoutes.js
+++ Backend/routes/creatorauthRoutes.js
@@ -1,16 +1,17 @@
-// creatorauthRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const { creatorsignupget, creatorsignuppost, creatorloginget, creatorloginpost, creatorlogoutget } = require('../controllers/creatorauthcontrollers');
+const { creatorsignupget, creatorsignuppost, creatorloginget, creatorloginpost, creatorlogoutget, updateTokens } = require('../controllers/creatorauthcontrollers');
+
+
+router.get('/', (req, res) => {
+    res.render('index'); 
+});
 
 router.get('/creatorsignup', creatorsignupget);
 router.post('/creatorsignup', creatorsignuppost);
 router.get('/creatorlogin', creatorloginget);
 router.post('/creatorlogin', creatorloginpost);
 router.get('/creatorlogout', creatorlogoutget);
-
-
-
+router.post('/updateTokens', updateTokens); 
 
 module.exports = router;
--- Backend/routes/userauthRoutes.js
+++ Backend/routes/userauthRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { usersignupget, usersignuppost, userloginget, userloginpost, userlogoutget } = require('../controllers/userauthcontrollers');
 
+// Define route for home page
+router.get('/', (req, res) => {
+    res.render('index'); // Assuming index.ejs is in your views directory
+});
+
 router.get('/usersignup', usersignupget);
 router.post('/usersignup', usersignuppost);
 router.get('/userlogin', userloginget);
@@ -10,8 +15,6 @@ router.get('/userlogout', userlogoutget);
 
 router.get('/userdashboard', (req, res) => {
     res.render('userdashboard', { username: res.locals.username });
-    res.render('userdashboard');
 });
-
 
 module.exports = router;
